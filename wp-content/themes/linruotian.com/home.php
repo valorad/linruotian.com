c0b70cd3f737ddeb3b8dbc508400ca7e1dfa7ca0
Valorad <wcxaaa@gmail.com>
2016-08-25 11:20:32 +0800

    initialize previous repo as wordpress theme

---
+++ wp-content/themes/linruotian.com/home.php
@@ -0,0 +1,224 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable = 0" name="viewport" />
+<title>Welcome to linruotian!</title>
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap.min.css" />
+
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/bootstrap-theme.min.css" />
+<link rel="stylesheet" href="<?php bloginfo('template_directory'); ?>/css/lrtHP.css" />
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery-2.2.0.min.js" ></script>
+<script src="<?php bloginfo('template_directory'); ?>/js/jquery.countdown.min.js"></script>
+<script type="text/javascript">
+	// $(function() {
+	// 	$('#divWin10CD').countdown('2016/07/30 15:00:00', function(event) {
+	//     $("#divWin10CD").html(event.strftime('%w weeks %d days %H:%M:%S'));
+	//   });
+	// })
+</script>
+<style type="text/css">
+	.transparent_class {   
+      filter:alpha(opacity=50);   
+      -moz-opacity:0.5;   
+      -khtml-opacity: 0.5;   
+      opacity: 0.5;
+}
+    .lrtCarTitle {
+    	background-color: rgba(0,0,0,0.5);
+    }
+
+</style>
+</head>
+
+<body>
+<div class="testtt">
+	
+	<script type="text/javascript">
+
+	</script>	
+</div>
+	<nav class="navbar navbar-default">
+		<div class="container-fluid">
+			<div class="navbar-header">
+				<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#lrtNavbarXS" aria-expanded="false" aria-controls="navbar" style="float:left;margin-left: 10px;">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar"></span>
+				</button>
+			</div>
+			<!-- Collect the nav links, forms, and other content for toggling -->
+			<div class="collapse navbar-collapse" id="lrtNavbarXS">
+				<ul class="nav">
+					<li class="active"><a href="#">Home</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Portal <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="#">FO4 map</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="#">Billy's steam account</a></li>
+							<li><a href="#">Steve's steam account</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+			<!-- nav bar for screens bigger than cell-phones -->
+			<div class="collapse navbar-collapse" id="lrtNavbar">
+				<ul class="nav nav-pills" style="margin-top: 3px;">
+					<li class="active"><a href="#">Home</a></li>
+					<li class="dropdown">
+						<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Portal <span class="caret"></span></a>
+						<ul class="dropdown-menu">
+							<li><a href="mapFO4.html">FO4 map</a></li>
+							<li role="separator" class="divider"></li>
+							<li><a href="http://steamcommunity.com/profiles/76561198043495159/">Billy's steam account</a></li>
+							<li><a href="http://steamcommunity.com/profiles/76561198069492267/">Steve's steam account</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+
+		</div>	
+	</nav>
+
+	<header class="container">
+		<div class="row">
+			<div class="col-md-6 col-md-push-6" style="margin: 0 auto">
+				<img class="img-circle center-block img-responsive" src="<?php bloginfo('template_directory'); ?>/Pic/SC2-1.jpg" style="height: 256px; width: 256px" alt="Avatar Billy">
+			</div>	
+			<div class="col-md-6 col-md-pull-6" style="margin: 0 auto; text-align:center">
+				<h1 style="    margin-top: 90px;    margin-bottom: 45px;">
+					Welcome to linruotian ! <br />
+					<small>A vault full of <mark>BDKs</mark></small>
+				</h1>
+			</div>
+		</div>
+	</header>
+
+	<main class="container">
+		<div class="row" id="divBillyVoice">
+			<blockquote>
+				No! Billy will never give you a single BDK!
+				<footer>Billy the infamous</footer>
+			</blockquote>			
+		</div>
+		<div class="row">
+			<div class="col-md-2"></div>
+			<div class="col-md-8">
+				<blockquote class="bg-warning">
+					<div style="text-align:center;"><a href="https://www.microsoftstore.com/store/msusa/en_US/pdp/Windows-10-Home/productID.319937100?ICID=Windows_Win10Home_ModE" target="_blank"><img class="img img-responsive" src="<?php bloginfo('template_directory'); ?>/Pic/windows.png" style="height:190px;margin:0px auto;"></a></div>
+					<!-- <div id="divWin10CD" style="font-size: 44px;"></div> -->
+					<footer style="font-size: 30px">Windows 10 only ￥888/$119.99!</footer>
+				</blockquote>
+			</div>
+		</div>
+
+		<div class="row">
+			<div class="col-md-12">
+				<div id="wcSlider" class="carousel slide" data-ride="carousel">
+					<!-- Indicators dot nav -->
+					<ol class="carousel-indicators">
+						<li data-target="#wcSlider" data-slide-to="0" class="active"></li>
+						<li data-target="#wcSlider" data-slide-to="1"></li>
+						<li data-target="#wcSlider" data-slide-to="2"></li>
+						<li data-target="#wcSlider" data-slide-to="3"></li>
+					</ol>
+					<!-- Wrappers for slides -->
+					<div class="carousel-inner" role="listbox">
+						<div class="item active">
+							<a href="https://www.facebook.com/groups/737083126434856/" target="_blank">
+								<img src="<?php bloginfo('template_directory'); ?>/Pic/GroupGDH.jpg" alt="We are Gaoduanhei!">
+							</a>
+							<div class="carousel-caption">
+								<h1 class="lrtCarTitle">We are Gaoduanhei!</h1>
+							</div>
+
+						</div>							
+						<div class="item">
+							<a href="https://github.com/wcxaaa/linruotian.com" target="_blank">
+								<img src="<?php bloginfo('template_directory'); ?>/Pic/githubsrc.png" alt="Checkout on github!">								
+							</a>
+							<div class="carousel-caption">
+								<h1 class="lrtCarTitle">Check it out on github!</h1>
+							</div>
+						</div>
+						<div class="item">
+							<a href="http://123.206.49.214/" target="_blank">
+								<img src="<?php bloginfo('template_directory'); ?>/Pic/wcNexusPos.png" alt="wcNexus coming soon!">								
+							</a>
+							<div class="carousel-caption">
+								<h1 class="lrtCarTitle">wcNexus coming soon!</h1>
+							</div>
+						</div>	
+						<div class="item">
+							<a href="mapFO4.html" target="_blank">
+								<img src="<?php bloginfo('template_directory'); ?>/Pic/FO4Map.png" alt="Fallout 4 map">								
+							</a>
+							<div class="carousel-caption">
+								<h1 class="lrtCarTitle">Fallout 4 map from Wikia</h1>
+							</div>
+						</div>											
+					</div>
+
+					<!-- controls or next&prev buttons -->
+					<a class="left carousel-control" href="#wcSlider" role="button" data-slide="prev">
+						<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+						<span class="sr-only">Previous</span>
+					</a>
+					<a class="right carousel-control" href="#wcSlider" role="button" data-slide="next">
+						<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+						<span class="sr-only">Next</span>
+					</a>					
+				</div>
+			</div>			
+		</div>
+
+		<div class="row" id="divTables">
+			<div class="col-xs-12 col-md-6">
+				<h2>wc Sites on linruotian.com:</h2>
+				<table class="table col-xs-12 col-md-6">
+					<thead style="font-weight: bolder;">
+						<td>Site</td> <td>Rate</td>
+					</thead>			
+					<tbody>
+						<tr>
+							<td><a href="HTML WC world/index.html">wc 5 JQ</a></td> <td>★★★★★</td>
+						</tr>
+						<tr>
+							<td><a href="mapFO4.html">mapFO4</a></td> <td>★★</td>
+						</tr>						
+					</tbody>
+				</table>
+			</div>
+			<div class="col-xs-12 col-md-6">
+				<h2>What Steve wants NOW:</h2>
+				<table class="table col-xs-12 col-md-6">
+					<thead style="font-weight: bolder;">
+						<td>CDK</td> <td>Price</td> <td>Addin Date</td>
+					</thead>				
+					<tbody class=".table-striped">
+						<tr>
+							<td><a href="http://store.steampowered.com/app/377160/" target="_blank"><del>Fallout 4</del></a></td> <td>￥199</td> <td>Gifted! Thank you!</td>
+						</tr>
+						<tr>
+							<td><a href="http://shop.ubi.com/store/ubiemea/en_IE/pd/productID.327112600" target="_blank">Far Cry Primal</a></td> <td>€59.99</td> <td>2015-12-5</td>
+						</tr>
+						<tr>
+							<td><a href="http://www.apple.com/shop/buy-iphone/iphone6s" target="_blank">iPhone 6S plus</a></td> <td>$749</td> <td>2016-1-15</td>
+						</tr>								
+					</tbody>
+				</table>
+			</div>				
+		</div>
+	</main>
+
+	<footer>
+		<script src="http://haiqiancun.com/file/demo/custom.modernizr.js"></script>
+		<script type="text/javascript">var cnzz_protocol = (("https:" == document.location.protocol) ? " https://" : " http://");document.write(unescape("%3Cspan id='cnzz_stat_icon_1257413436'%3E%3C/span%3E%3Cscript src='" + cnzz_protocol + "s4.cnzz.com/z_stat.php%3Fid%3D1257413436%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));</script>
+	</footer>
+
+	<!--  JS files At the bottom -->
+	<script src="<?php bloginfo('template_directory'); ?>/js/bootstrap.min.js"></script>
+
+</body>
+</html>
